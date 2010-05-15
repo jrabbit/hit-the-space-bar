a4162bc3052642562702aedbcd09c06352a2df20
Jrabbit <jackjrabbit+github@gmail.com>
2010-05-15 14:47:00 -0500

    now with more competition!

--- space.py
+++ space.py
@@ -4,12 +4,19 @@ import urwid
 txt = urwid.Text("push the spacebar!")
 #Should I even add physics?
 physics_fact=["f=ma","Vx=-9.8m/s^2 + V0*COSTheta"]
+score = 0
 #reddit meme goes here
 def show_or_exit(input):
+	global score
 	if input != ' ':
 		# You didn't press space!
 		raise urwid.ExitMainLoop()
-	txt.set_text("you pushed the spacebar!")
+	if score < 1:
+		message = "you pushed the spacebar!"
+	else:
+		message = "you pushed the spacebar " + str(score) + " times!"
+	txt.set_text(message)
+	score = score + 1
 
 fill = urwid.Filler(txt, 'top')
 loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
