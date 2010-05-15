ee80c838699b33ec1322e34d144ed9ddc10886a3
Jrabbit <jackjrabbit+github@gmail.com>
2010-05-15 13:07:20 -0500

    Jokes for nerds. [Hello fellow sports-racers]

--- space.py
+++ space.py
@@ -2,6 +2,9 @@
 # 2010. GPL v3 and all later versions. Jrabbit.
 import urwid
 txt = urwid.Text("push the spacebar!")
+#Should I even add physics?
+physics_fact=["f=ma","Vx=-9.8m/s^2 + V0*COSTheta"]
+#reddit meme goes here
 def show_or_exit(input):
 	if input != ' ':
 		# You didn't press space!
