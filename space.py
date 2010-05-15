eeacc5d38a8c0315a45dd4a3316b55bc4197c0c1
Jrabbit <jackjrabbit+github@gmail.com>
2010-05-15 12:51:46 -0500

    improved game mechanics

--- space.py
+++ space.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
+# 2010. GPL v3 and all later versions. Jrabbit.
 import urwid
-import time
 txt = urwid.Text("push the spacebar!")
 def show_or_exit(input):
 	if input != ' ':
-		txt.set_text("You didn't press space!")
-		time.sleep(2) 
+		# You didn't press space!
 		raise urwid.ExitMainLoop()
 	txt.set_text("you pushed the spacebar!")
 
 fill = urwid.Filler(txt, 'top')
 loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
 loop.run()
+print "You didn't press space!"
