4d6a15b4b43f9437b7bbed553f22f3279b0af335
Jack Laxson <jackjrabbit@gmail.com>
2017-04-01 02:08:22 -0400

    scan generally is space delimited... we need a fancy library

--- htsb/htsb.go
+++ htsb/htsb.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"fmt"
+	// "bufio"
+	// "os"
+	"github.com/nsf/termbox-go"
+
 )
+
 
 func main() {
 	fmt.Println("Welcome to hit the spacebar 2017 GOTY edition")
-	var x string
-	for {
-		fmt.Scan(&x)
-		if x == " " {
-			fmt.Println("space detected")
-		}
-		fmt.Println(x)
+	// var x string
+	// var score int
+	// scanner := bufio.NewScanner(os.Stdin)
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
 	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputAlt | termbox.InputMouse)
+mainloop:
+	for{
+		return
+	}
+
+	// for scanner.Scan() {
+		
+	// 	x = scanner.Text()
+	// 	if x == " " {
+	// 		fmt.Println("space detected")
+	// 		score += 1
+	// 	} else {
+	// 		fmt.Println()
+	// 	}
+	// 	fmt.Println("your score:", score)
+	// }
 }
