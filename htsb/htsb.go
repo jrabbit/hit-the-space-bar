9b4e60481da0b531f95e14c852e6205e2379f925
Jack Laxson <jackjrabbit@gmail.com>
2017-04-01 00:57:35 -0400

    begin work on 2017 GOTY edition

---
+++ htsb/htsb.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to hit the spacebar 2017 GOTY edition")
+	var x string
+	for true {
+		fmt.Scan(&x)
+		fmt.Println(x)
+	}
+}
