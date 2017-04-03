f00b2e15e47a39310039de910d379c470b2d9941
Jack Laxson <jackjrabbit@gmail.com>
2017-04-03 14:39:40 -0400

    redis support

--- src/server/server.go
+++ src/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-martini/martini"
+	"github.com/go-redis/redis"
 	"net/http"
 )
 
@@ -18,19 +19,54 @@ func root() string {
 	return "<h1>Hit the spacebar scoreboard server</h1>"
 }
 
-func scoreboard() string {
-	return "<body><ol><li>JEB - 9001</li></ol></body>"
+func getBoard(client *redis.Client) map[string]int64 {
+	scores := make(map[string]int64)
+	iter := client.Scan(0, "", 0).Iterator()
+	for iter.Next() {
+		ret, err := client.Get(iter.Val()).Int64()
+		if err != nil {
+			panic(err)
+		}
+		scores[iter.Val()] = ret
+		fmt.Println(iter.Val())
+	}
+	if err := iter.Err(); err != nil {
+		panic(err)
+	}
+	return scores
 }
 
-func postscore(req *http.Request) string {
-	fmt.Println(req)
+func scoreboard(client *redis.Client) string {
+	scores := getBoard(client)
+	var lines string
+	lines = "<html><body><ol><li> JEB -  9001 </li>"
+	for key, value := range scores {
+		lines += fmt.Sprintf("<li> %s -  %d </li>", key, value)
+	}
+	lines += "</ol></body></html>"
+	return lines
+}
+
+func postScore(req *http.Request) string {
+	form := req.PostForm
+	fmt.Println(form)
 	return "OK"
 }
 
 func main() {
 	m := martini.Classic()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	// db := &client
+	m.Map(client)
 	m.Get("/", root)
 	m.Get("/scoreboard", scoreboard)
-	m.Post("/scoreboard/submit", postscore)
+	m.Post("/scoreboard/submit", postScore)
 	m.Run()
 }
